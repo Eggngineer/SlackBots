a62176e2f0bdff9ac5e9437de75a26e255c9553d
Eggngineer <yama_shin1999@icloud.com>
2022-09-06 07:06:39 +0900

    chenge message for slack

--- say_hi.py
+++ say_hi.py
@@ -1,5 +1,5 @@
 import os
 from slack_sdk.web import WebClient
 client = WebClient(token=os.environ["SLACK_API_TOKEN"])
-response = client.chat_postMessage(text=":wave: こんにちは！", channel="#random")
+response = client.chat_postMessage(text=":wave: SlackBotリポジトリが更新されました！", channel="#random")
 print(response)
